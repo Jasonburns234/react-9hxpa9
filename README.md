1f364a04323884f3a9ba361bd2679bf34410caa2
Jasonburns234 <67003071+Jasonburns234@users.noreply.github.com>
2023-08-18 15:35:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-9hxpa9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sfqekt)
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Header(props) {
+  let [clock, setClock] = React.useState(Date().toLocaleString());
+
+  setInterval(() => {
+    setClock(Date().toLocaleString());
+  }, 1000);
+
+  return (
+    <h1 className="orange">
+      Hello {props.name} {clock}
+    </h1>
+  );
+}
